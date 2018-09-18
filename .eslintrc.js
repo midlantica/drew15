61e4407815eb62b26b9af098c72d9236a2a7c600
Andrew Harper <druharper@gmail.com>
2018-09-18 12:58:15 -0500

    Indent linter fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
   'rules': {
     // allow paren-less arrow functions
     'arrow-parens': 0,
+    // INDENT DEFAULT AT 2 SPACES
+    "indent": ["error", 2],
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
