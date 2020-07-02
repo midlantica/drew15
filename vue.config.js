fa363e23d6321999eb32daff284d8a0f4e923d70
Drew Harper <druharper@gmail.com>
2020-07-02 16:18:43 -0500

    scss source map nah - save

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,53 @@
 // And:
 // https://webpack.js.org/loaders/sass-loader/
 
+const path = require('path')
+
 module.exports = {
+  parser: 'sugarss',
+  plugins: {
+    'postcss-import': {},
+    'postcss-preset-env': {},
+    'cssnano': {}
+  },
   css: {
     loaderOptions: {
       sass: {
-        prependData: '@import "@/assets/css/main.scss";'
-      }
+        prependData: '@import "@/assets/css/main.scss";',
+        sourceMap: true,
+        sassOptions: {
+          outputStyle: 'compressed'
+        }
+      },
+      test: /\.css/,
+      use: [
+        { loader: 'style-loader', options: { sourceMap: true } },
+        { loader: 'css-loader', options: { sourceMap: true } },
+        { loader: 'postcss-loader', options: { sourceMap: true } },
+        { loader: 'sass-loader', options: { sourceMap: true } }
+      ]
     }
   }
 }
+
+// module.exports = {
+//   configureWebpack: config => {
+//     if (process.env.NODE_ENV === 'production') {
+//       css: {
+//         loaderOptions: {
+//           sass: {
+//             prependData: '@import "@/assets/css/main.scss";'
+//           }
+//         }
+//       }
+//     } else {
+//       css: {
+//         loaderOptions: {
+//           sass: {
+//             prependData: '@import "@/assets/css/main.scss";'
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
