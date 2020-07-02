e258c60ad3121b6ea8eabebad4ac24692c3bfff0
Drew Harper <druharper@gmail.com>
2020-07-02 18:36:44 -0500

    cleaning out node packages save

--- vue.config.js
+++ vue.config.js
@@ -1,55 +1,16 @@
+/* eslint-disable */
 // IMPORT INITIAL SCSS
 // https://css-tricks.com/how-to-import-a-sass-file-into-every-vue-component-in-an-app/
 // And:
 // https://webpack.js.org/loaders/sass-loader/
-
-const path = require('path')
-
+// CSS source maps only work on Chrome
 module.exports = {
-  parser: 'sugarss',
-  plugins: {
-    'postcss-import': {},
-    'postcss-preset-env': {},
-    'cssnano': {}
-  },
   css: {
+    sourceMap: true,
     loaderOptions: {
       sass: {
-        prependData: '@import "@/assets/css/main.scss";',
-        sourceMap: true,
-        sassOptions: {
-          outputStyle: 'compressed'
-        }
-      },
-      test: /\.css/,
-      use: [
-        { loader: 'style-loader', options: { sourceMap: true } },
-        { loader: 'css-loader', options: { sourceMap: true } },
-        { loader: 'postcss-loader', options: { sourceMap: true } },
-        { loader: 'sass-loader', options: { sourceMap: true } }
-      ]
+        prependData: '@import "@/assets/css/main.scss";'
+      }
     }
   }
 }
-
-// module.exports = {
-//   configureWebpack: config => {
-//     if (process.env.NODE_ENV === 'production') {
-//       css: {
-//         loaderOptions: {
-//           sass: {
-//             prependData: '@import "@/assets/css/main.scss";'
-//           }
-//         }
-//       }
-//     } else {
-//       css: {
-//         loaderOptions: {
-//           sass: {
-//             prependData: '@import "@/assets/css/main.scss";'
-//           }
-//         }
-//       }
-//     }
-//   }
-// }
