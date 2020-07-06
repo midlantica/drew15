393540f34754cb0d543fe7c0ee1194d8003650e0
Drew Harper <druharper@gmail.com>
2020-07-06 12:43:02 -0500

    VueCarousel ignore in urgecss

--- .postcssrc.js
+++ .postcssrc.js
@@ -12,7 +12,7 @@ module.exports = {
         const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
         return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
       },
-      whitelist: ['html', 'body'],
+      whitelist: ['html', 'body', 'VueCarousel'],
       whitelistPatterns: [
         /-(leave|enter|appear)(|-(to|from|active))$/,/^(?!(|.*?:)cursor-move).+-move$/,/^router-link(|-exact)-active$/,/data-v-.*/
       ]
