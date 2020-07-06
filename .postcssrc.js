c89e6d9f5f33b8b86aeec9c3171485ff59cf6104
Drew Harper <druharper@gmail.com>
2020-07-06 13:06:33 -0500

    save purge

--- .postcssrc.js
+++ .postcssrc.js
@@ -12,7 +12,7 @@ module.exports = {
         const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
         return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
       },
-      whitelist: ['html', 'body', 'VueCarousel'],
+      whitelist: ['html', 'body', '.VueCarousel', '/^VueCarousel$/', '/^druCarousel$/'],
       whitelistPatterns: [
         /-(leave|enter|appear)(|-(to|from|active))$/,/^(?!(|.*?:)cursor-move).+-move$/,/^router-link(|-exact)-active$/,/data-v-.*/
       ]
