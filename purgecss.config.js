441bd610d2db98e88572b452c3f595f5c014d0e9
Drew Harper <druharper@gmail.com>
2020-07-07 18:48:19 -0500

    new purgecss package

--- purgecss.config.js
+++ purgecss.config.js
@@ -5,5 +5,16 @@ module.exports = {
   ],
   css: [
     'dist/css/*.css'
+  ],
+  defaultExtractor (content) {
+    const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
+    return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+  },
+  whitelist: [],
+  whitelistPatterns: [
+    /-(leave|enter|appear)(|-(to|from|active))$/,
+    /^(?!(|.*?:)cursor-move).+-move$/,
+    /^router-link(|-exact)-active$/,
+    /data-v-.*/
   ]
 }
