fc251c228786621254051120c21eadfd75b42927
Drew Harper <druharper@gmail.com>
2020-07-20 17:31:41 -0500

    ani clear out save

--- src/template-bourbon/components/HeadAndCopy.vue
+++ src/template-bourbon/components/HeadAndCopy.vue
@@ -38,14 +38,6 @@
 
     }
 
-    .jelloHorizontal:hover {
-      -webkit-animation: jelloHorizontal 0.9s both !important;
-      animation: jelloHorizontal 0.9s both !important;
-      animation-delay: initial !important;
-      animation-play-state: running;
-      cursor: pointer;
-    }
-
     h1 {
       display: inline-block;
       font-family: $font-subhead;
