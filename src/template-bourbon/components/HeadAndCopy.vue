d0ae6c2492fa2de62dacd8fde87a003b6c0badfa
Drew Harper <dmail@gmail.com>
2021-03-29 12:44:01 -0500

    animation of drew head good

--- src/template-bourbon/components/HeadAndCopy.vue
+++ src/template-bourbon/components/HeadAndCopy.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="marT0 marL1 marRH">
     <div class="headBlock">
-      <transition enter-active-class="jelloHorizontal">
-        <router-link :to="{ name: 'ExtrasBourbon' }" class="drewhead jelloHorizontal" />
-      </transition>
+      <router-link :to="{ name: 'ExtrasBourbon' }" class="drewhead jelloHorizontal" />
       <h1>When UX design is done right the design seems inevitable</h1>
     </div>
     <p>Whether it’s a prototype, a responsive website, a wireframe, an interactive <span class="smallCaps">PDF</span>, a new mobile app <span class="smallCaps">UI</span>, a quick skin job, a few more buttons, a dialog box, filling up a bucket, or getting in the trenches with  <span class="smallCaps">HTML</span> and <span class="smallCaps">CSS</span>, I can deliver. I don’t let ‘design’ get in the way. Oh and I’m a coder groupie 😃</p>
