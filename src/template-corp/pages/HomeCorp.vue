3688d43b6ff59e3bd1c51ec0354f17f91f2a8e5c
Drew Harper <druharper@gmail.com>
2021-04-09 16:36:34 -0500

    netlify re

--- src/template-corp/pages/HomeCorp.vue
+++ src/template-corp/pages/HomeCorp.vue
@@ -42,7 +42,7 @@
 
 <style lang="scss" scoped>
   @import "@/template-corp/assets/css/corp-main.scss";
-  // @import url('https://fonts.googleapis.com/css2?family=Alegreya+SC:ital,wght@0,400;0,500;1,400;1,500&family=Alegreya:ital,wght@0,400;0,500;1,400;1,500&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Alegreya+SC:ital,wght@0,400;0,500;1,400;1,500&family=Alegreya:ital,wght@0,400;0,500;1,400;1,500&display=swap');
 
   .about {
     background: white;
