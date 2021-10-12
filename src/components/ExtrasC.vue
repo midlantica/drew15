682553c953231d8b95798283c37b3fddb448da85
Drew Harper <druharper@gmail.com>
2021-10-12 12:14:38 -0500

    My pic ain't showing! Grr.

--- src/components/ExtrasC.vue
+++ src/components/ExtrasC.vue
@@ -5,10 +5,10 @@
 
       <div class="flexxy flexRowWrap center-flex align-content-start">
 
-        <a href="/img/portfolio/extras/zander_pg.png" target="_BLANK">
-          <img class="portThumbs" src="/img/portfolio/extras/Zander_pg_sm.png" />
+        <a href="/img/portfolio/extras/Zzander.png" target="_BLANK">
+          <img class="portThumbs" src="/img/portfolio/extras/Zzander_sm@2x.png" />
           <div class="text-center">
-            <p>Zander UX</p>
+            <p>A Service Request page</p>
           </div>
         </a>
 
